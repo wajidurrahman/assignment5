1625e8d875b55e459b81faa535e4d22556cc987b
Wajidur Rahman <wrfuad0@gmail.com>
2021-02-10 22:58:35 +0600

    change index html

--- api.js
+++ api.js
@@ -1,18 +1,15 @@
+const searchButton = meals => {
+    const searchInput = document.getElementById('search-Input').value;
+    // console.log(searchInput);
+    const url = `https://www.themealdb.com/api/json/v1/1/list.php?i=${meals}`;
+    // console.log(url);
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data));
+}
 
-fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
-.then(res => res.json())
-.then(data => allMealList(data))
+const displayMealDetails = () => {
+    mealContainer = document.getElementById('search-meal');
+    mealContainer.innerHTML = '',
 
-const categories = document.getElementById('searchBtn');
-categories.addEventListener('click', () => {
-    const searchMeal = document.getElementById('searchInput').value;
-    getMealDetails(searchMeal)
-    document.getElementById('mealName').innerText = data.categories;
-    document.getElementById('images').getImageData(`https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg/preview`);
-    
-})
-const changeMealList = document.getElementById('mealName').addEventListener('click', () => {
-   listItem = document.getElementById('strCategoryThumb').innerText;
-   
-
-})
+}
