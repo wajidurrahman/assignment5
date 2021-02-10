3fa02dd4ee5fd2988f923aed05a6b45cb730e5d5
Wajidur Rahman <wrfuad0@gmail.com>
2021-02-10 23:59:28 +0600

    search btn workig

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
-const searchButton = meals => {
+function searchButton(){
     const searchInput = document.getElementById('search-Input').value;
     // console.log(searchInput);
-    const url = `https://www.themealdb.com/api/json/v1/1/list.php?i=${meals}`;
+    const url = `https://www.themealdb.com/api/json/v1/1/list.php?i=${searchInput}`;
     // console.log(url);
     fetch(url)
     .then(res => res.json())
@@ -9,7 +9,8 @@ const searchButton = meals => {
     displayMealDetails()
 }
 
-const displayMealDetails = () => {
+function displayMealDetails(){
     mealContainer = document.getElementById('search-meal');
-    mealContainer.innerHTML = '',
+    mealContainer.innerHTML = '';
+
 }
